60aac1c2d5f42fcef07f6cc38982a8b49b03bdd4
George <georgehm@pm.me>
2021-11-18 14:08:10 +0000

    Add enums for output type and field

---
+++ src/php/enum/OutputField.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Enum;
+
+class EnumOutputField extends EnumAbstract
+{
+    const OUTPUT_HOSTNAME = 'hostname';
+
+    const FIELDS = [
+        self::OUTPUT_HOSTNAME
+    ];
+}
---
+++ src/php/enum/OutputType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Enum;
+
+class EnumOutputType extends EnumAbstract
+{
+    const OUTPUT_COUNT = 'count';
+
+    const VALUES = [
+        self::OUTPUT_COUNT
+    ];
+}
