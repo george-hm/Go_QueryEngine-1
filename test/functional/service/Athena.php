c9060a1f677fca9e6e903cf71a2e77a3edec35b2
George <georgehm@pm.me>
2021-11-19 12:42:57 +0000

    Add functional test for athena

---
+++ test/functional/service/Athena.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GoQueryEngine\Service;
+
+use GoQueryEngine\Enum\EnumOutputField;
+use GoQueryEngine\Enum\EnumOutputType;
+use GoQueryEngine\Model\Output\ModelOutputAbstract;
+use GoQueryEngine\Model\Where\ModelWhereAbstract;
+
+class FunctionalTest_Athena extends \GoQueryEngine\Functional_TestCase
+{
+    private static $strBaseURL = 'http://localhost/canddi/';
+    private static $strToken = '12345';
+
+    public static function testLookup()
+    {
+        $enumType = new EnumOutputType(EnumOutputType::OUTPUT_COUNT);
+        $modelOutput = ModelOutputAbstract::create($enumType);
+        $enumField = new EnumOutputField(EnumOutputField::OUTPUT_HOSTNAME);
+        $modelWhere = new ModelWhereAbstract($enumField);
+        $modelWhere->equals('foo');
+
+        $serviceAthena = ServiceAthena::getInstance(self::$strBaseURL);
+        $serviceAthena->setToken(self::$strToken)
+            ->setOutput($modelOutput)
+            ->addWhere($modelWhere)
+            ->setBInternal(true)
+            ->setBUnique(true)
+            ->lookup();
+    }
+}
