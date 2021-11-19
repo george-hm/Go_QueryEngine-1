03aeec3272fb965ed3d1fec69210823c02ea1dd2
George <georgehm@pm.me>
2021-11-19 12:03:38 +0000

    Add constructor and type getter

--- src/php/model/output/ModelOutputAbstract.php
+++ src/php/model/output/ModelOutputAbstract.php
@@ -7,13 +7,22 @@ use Exception;
 
 abstract class ModelOutputAbstract
 {
+
+    public function __construct(EnumOutputType $enumOutputType) {
+        $this->_enumOutputType = $enumOutputType;
+    }
+
     public static function create(EnumOutputType $enumOutputType) {
         switch ($enumOutputType->getId()) {
             case EnumOutputType::OUTPUT_COUNT:
-                return new ModelOutputAbstract();
+                return new ModelOutputAbstract($enumOutputType);
             default:
                 throw new Exception('Invalid output type');
         }
+    }
+
+    public function getType() {
+        return $this->_enumOutputType->getId();
     }
 
     public abstract function setField();
