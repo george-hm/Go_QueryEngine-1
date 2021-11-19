089194099a88ec972c8d9498d3b746c711657ace
George <georgehm@pm.me>
2021-11-19 10:45:20 +0000

    Check type on where abstract

--- src/php/model/where/ModelWhereAbstract.php
+++ src/php/model/where/ModelWhereAbstract.php
@@ -2,6 +2,17 @@
 
 namespace Model\Where;
 
-class ModelWhereAbstract {
+use Enum\EnumOutputField;
+use Exception;
 
+class ModelWhereAbstract {
+    public function __construct(EnumOutputField $enumOutputField) {
+        switch ($enumOutputField->getType()) {
+            case EnumOutputField::TYPE_STRING:
+                # code...
+                break;
+            default:
+                throw new Exception('Invalid output type');
+        }
+    }
 }
