48626c30349cea50238be0e3d85dbead8f81987f
George <georgehm@pm.me>
2021-11-19 15:43:22 +0000

    Test model output

---
+++ test/php/model/output/ModelOutputAbstractTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace GoQueryEngine\Model\Output;
+
+use Exception;
+use GoQueryEngine\Enum\EnumOutputType;
+use Mockery;
+
+class ModelOutputAbstractTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateNoMapping()
+    {
+        $this->expectException(Exception::class);
+
+        $mockEnumOutputType = Mockery::mock(EnumOutputType::class)
+            ->shouldReceive('getId')
+            ->andReturn('invalidTestId')
+            ->mock();
+
+        ModelOutputAbstract::create($mockEnumOutputType);
+    }
+}
