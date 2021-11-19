42079dc25bbe2cdcd341d65fff1756bd6ab2aae2
George <georgehm@pm.me>
2021-11-19 10:45:08 +0000

    Add types to output field

--- src/php/enum/OutputField.php
+++ src/php/enum/OutputField.php
@@ -4,9 +4,20 @@ namespace Enum;
 
 class EnumOutputField extends EnumAbstract
 {
+    const TYPE_STRING = 'string';
+
     const OUTPUT_HOSTNAME = 'hostname';
 
     const FIELDS = [
         self::OUTPUT_HOSTNAME
     ];
+
+    const FIELD_TYPES = [
+        self::OUTPUT_HOSTNAME => self::TYPE_STRING
+    ];
+
+    public function getType()
+    {
+        return self::FIELD_TYPES[$this->getId()];
+    }
 }
