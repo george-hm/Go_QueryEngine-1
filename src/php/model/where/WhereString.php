862df1dfc25e0c06d24679340134e6c2c3758500
George <georgehm@pm.me>
2021-11-22 14:29:51 +0000

    Move constructor to parent and return employee

--- src/php/model/where/WhereString.php
+++ src/php/model/where/WhereString.php
@@ -10,15 +10,6 @@ class ModelWhereString extends ModelWhereAbstract
 {
     use TraitOperatorEquals;
 
-    public function __construct(EnumOutputField $enumOutputField)
-    {
-        if ($enumOutputField->getType() !== EnumOutputField::TYPE_STRING) {
-            throw new Exception('Invalid output type');
-        }
-        $this->_enumOutputField = $enumOutputField;
-        $this->_arrWhere['name'] = $enumOutputField->getId();
-    }
-
     public function in(array $arrValues)
     {
         foreach ($arrValues as $mixCurrentValue) {
