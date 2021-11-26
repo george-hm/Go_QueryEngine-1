67e07b896a3af1211dcfb3c062c0be94ad15519e
George <georgehm@pm.me>
2021-11-26 17:08:41 +0000

    Return this

--- src/php/model/where/WhereLocation.php
+++ src/php/model/where/WhereLocation.php
@@ -10,23 +10,30 @@ class ModelWhereLocation extends ModelWhereAbstract
     public function setRadius(int $intRadius) {
         $this->_arrWhere[EnumOperators::VALUE_WITHIN]
             [EnumOperators::VALUE_WITHIN_RADIUS] = $intRadius;
+
+        return $this;
     }
 
     public function setLat(float $floatLat) {
         $this->_arrWhere[EnumOperators::VALUE_WITHIN]
             [EnumOperators::VALUE_WITHIN_LAT] = $floatLat;
+
+        return $this;
     }
 
     public function setLng(float $floatLng) {
         $this->_arrWhere[EnumOperators::VALUE_WITHIN]
             [EnumOperators::VALUE_WITHIN_LNG] = $floatLng;
+
+        return $this;
     }
 
     public function toArray()
     {
+        error_log(print_r($this->_arrWhere, true));
         if (
             !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]) ||
-            !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]['radius']) ||
+            !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN][EnumOperators::VALUE_WITHIN_RADIUS]) ||
             !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]
                 [EnumOperators::VALUE_WITHIN_LAT]) ||
             !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]
