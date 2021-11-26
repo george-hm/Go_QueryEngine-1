eae53f6a861cb60be650212b3b68fc0752ac45b0
George <georgehm@pm.me>
2021-11-26 17:17:34 +0000

    Fix linting

--- src/php/model/where/WhereLocation.php
+++ src/php/model/where/WhereLocation.php
@@ -7,21 +7,24 @@ use GoQueryEngine\Enum\EnumOperators;
 
 class ModelWhereLocation extends ModelWhereAbstract
 {
-    public function setRadius(int $intRadius) {
+    public function setRadius(int $intRadius)
+    {
         $this->_arrWhere[EnumOperators::VALUE_WITHIN]
             [EnumOperators::VALUE_WITHIN_RADIUS] = $intRadius;
 
         return $this;
     }
 
-    public function setLat(float $floatLat) {
+    public function setLat(float $floatLat)
+    {
         $this->_arrWhere[EnumOperators::VALUE_WITHIN]
             [EnumOperators::VALUE_WITHIN_LAT] = $floatLat;
 
         return $this;
     }
 
-    public function setLng(float $floatLng) {
+    public function setLng(float $floatLng)
+    {
         $this->_arrWhere[EnumOperators::VALUE_WITHIN]
             [EnumOperators::VALUE_WITHIN_LNG] = $floatLng;
 
@@ -30,15 +33,21 @@ class ModelWhereLocation extends ModelWhereAbstract
 
     public function toArray()
     {
-        error_log(print_r($this->_arrWhere, true));
+        // make sure all the values have been set
         if (
             !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]) ||
-            !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]
-                [EnumOperators::VALUE_WITHIN_RADIUS]) ||
-            !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]
-                [EnumOperators::VALUE_WITHIN_LAT]) ||
-            !isset($this->_arrWhere[EnumOperators::VALUE_WITHIN]
-                [EnumOperators::VALUE_WITHIN_LNG])
+            !isset(
+                $this->_arrWhere[EnumOperators::VALUE_WITHIN]
+                    [EnumOperators::VALUE_WITHIN_RADIUS]
+            ) ||
+            !isset(
+                $this->_arrWhere[EnumOperators::VALUE_WITHIN]
+                    [EnumOperators::VALUE_WITHIN_LAT]
+            ) ||
+            !isset(
+                $this->_arrWhere[EnumOperators::VALUE_WITHIN]
+                    [EnumOperators::VALUE_WITHIN_LNG]
+            )
         ) {
             throw new Exception(
                 'You must set the radius, lat and lng before calling toArray()'
