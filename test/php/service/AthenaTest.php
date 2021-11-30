200e3990ea9d5eef7e43b77c80a5fa2a91f5357e
George <georgehm@pm.me>
2021-11-30 10:27:15 +0000

    Add more test cases for different outputs

--- test/php/service/AthenaTest.php
+++ test/php/service/AthenaTest.php
@@ -4,6 +4,8 @@ namespace GoQueryEngine\Service;
 
 use Exception;
 use GoQueryEngine\Model\Output\ModelOutputCount;
+use GoQueryEngine\Model\Output\ModelOutputList;
+use GoQueryEngine\Model\Output\ModelOutputPivot;
 use GoQueryEngine\Model\Where\ModelWhereString;
 
 class ServiceAthenaTest extends \TestCase
@@ -113,6 +115,134 @@ class ServiceAthenaTest extends \TestCase
             'unique_companies' => true,
             'callback_url' => $strCallbackURL,
             'json' => 1
+        ];
+
+        $mockGuzzle = \Mockery::mock('GuzzleHttp\Client')
+            ->shouldReceive('request')
+            ->once()
+            ->with(
+                'POST',
+                ServiceAthena::c_strURL_Athena,
+                ['json' => $arrExpectedRequest]
+            )
+            ->mock();
+
+        $serviceAthena->injectGuzzle($mockGuzzle);
+
+        $serviceAthena->lookup();
+    }
+
+    function testLookupTypePivot()
+    {
+        $strBaseURL = 'http://localhost:8080';
+        $serviceAthena = ServiceAthena::getInstance($strBaseURL);
+
+        $strOutputType = 'pivot';
+        $strColumn = 'testColumn';
+        $strRow = 'testRow';
+        $mockModelOutputAbstract = \Mockery::mock(ModelOutputPivot::class)
+            ->shouldReceive('getType')
+            ->times(3)
+            ->andReturn($strOutputType)
+            ->shouldReceive('getColumn')
+            ->andReturn($strColumn)
+            ->shouldReceive('getRow')
+            ->andReturn($strRow)
+            ->mock();
+
+        $serviceAthena->setOutput($mockModelOutputAbstract);
+        $serviceAthena->setToken('testToken');
+
+        $arrMockWhereReturn = [
+            'name' => 'testName',
+            'equals' => 1
+        ];
+        $mockModelWhereString = \Mockery::mock(ModelWhereString::class)
+            ->shouldReceive('toArray')
+            ->withNoArgs()
+            ->andReturn($arrMockWhereReturn)
+            ->once()
+            ->mock();
+
+        $serviceAthena->addWhere($mockModelWhereString);
+
+        $strCallbackURL = 'http://localhost:8080/callback';
+        $serviceAthena->setCallbackURL($strCallbackURL)
+            ->setBUnique(true)
+            ->setBInternal(true);
+
+        $arrExpectedRequest = [
+            'type' => $strOutputType,
+            'where' => [$arrMockWhereReturn],
+            'use_internal' => true,
+            'unique_companies' => true,
+            'callback_url' => $strCallbackURL,
+            'json' => 1,
+            'column' => $strColumn,
+            'row' => $strRow
+        ];
+
+        $mockGuzzle = \Mockery::mock('GuzzleHttp\Client')
+            ->shouldReceive('request')
+            ->once()
+            ->with(
+                'POST',
+                ServiceAthena::c_strURL_Athena,
+                ['json' => $arrExpectedRequest]
+            )
+            ->mock();
+
+        $serviceAthena->injectGuzzle($mockGuzzle);
+
+        $serviceAthena->lookup();
+    }
+
+    function testLookupTypeList()
+    {
+        $strBaseURL = 'http://localhost:8080';
+        $serviceAthena = ServiceAthena::getInstance($strBaseURL);
+
+        $strOutputType = 'list';
+        $strColumn = 'testColumn';
+        $mockModelOutputAbstract = \Mockery::mock(ModelOutputList::class)
+            ->shouldReceive('getType')
+            ->times(3)
+            ->andReturn($strOutputType)
+            ->shouldReceive('getColumn')
+            ->andReturn($strColumn)
+            ->mock();
+
+        $serviceAthena->setOutput($mockModelOutputAbstract);
+        $serviceAthena->setToken('testToken');
+
+        $arrMockWhereReturn = [
+            'name' => 'testName',
+            'equals' => 1
+        ];
+        $mockModelWhereString = \Mockery::mock(ModelWhereString::class)
+            ->shouldReceive('toArray')
+            ->withNoArgs()
+            ->andReturn($arrMockWhereReturn)
+            ->once()
+            ->mock();
+
+        $serviceAthena->addWhere($mockModelWhereString);
+
+        $strCallbackURL = 'http://localhost:8080/callback';
+        $serviceAthena->setCallbackURL($strCallbackURL)
+            ->setBUnique(true)
+            ->setBInternal(true);
+
+        $arrExpectedRequest = [
+            'type' => $strOutputType,
+            'where' => [$arrMockWhereReturn],
+            'use_internal' => true,
+            'unique_companies' => true,
+            'callback_url' => $strCallbackURL,
+            'json' => 1,
+            'out_fields' => [
+                $strColumn
+            ]
         ];
 
         $mockGuzzle = \Mockery::mock('GuzzleHttp\Client')
